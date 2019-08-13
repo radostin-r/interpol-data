f1497d71796832f04e9894395bb71963f3513d3e
Radostin Radoev <r.rashk@gmail.com>
2019-08-13 18:49:14 +0300

    generate data file code first iter

--- interpolcrawler/generate_data/output_data.py
+++ interpolcrawler/generate_data/output_data.py
@@ -1,5 +1,56 @@
+import os
 
 
-def generate_data():
-    pass
+def generate_data(format=None, outputfile=None, sex=None, yob=None, name=None):
+    # file_extension is not json and format is json raise error
+    # if file_extension is not xml and format is xml raise error
+    split_file_name = outputfile.split('.')
+
+    if not outputfile:
+        # get data from db
+        # print data to stdout
+        if format != 'xml':
+            # print json
+        else:
+            #print xml
+
+    elif (len(split_file_name) > 1 and split_file_name[-1] == 'json') or len(split_file_name) == 1:
+        create_json_file(split_file_name, sex, yob, name)
+    elif format == 'xml' or len(split_file_name) > 1 and split_file_name[-1] == 'xml':
+        create_xml_file(split_file_name, sex, yob, name)
+    else:
+        raise Exception("Wrong file extension or format")
+
+
+def create_json_file(file_name_list=None, sex=None, yob=None, name=None):
+    file_name = None
+    if len(file_name_list) > 1:
+        file_name = '.'.join(file_name_list)
+    else:
+        file_name = file_name_list[0] + '.json'
+
+    new_file_path = os.path.join(os.getcwd(), '..', '..', file_name)
+    new_file = open(new_file_path, 'w')
+
+    # get person data
+
+    # write to file
+    new_file.write()
+
+
+def create_xml_file(file_name_list=None, sex=None, yob=None, name=None):
+    file_name = None
+    if len(file_name_list) > 1:
+        file_name = '.'.join(file_name_list)
+    else:
+        file_name = file_name_list[0] + '.xml'
+
+    new_file_path = os.path.join(os.getcwd(), '..', '..', file_name)
+    new_file = open(new_file_path, 'w')
+
+    # get person data
+
+    # write to file
+    new_file.write()
+
 
